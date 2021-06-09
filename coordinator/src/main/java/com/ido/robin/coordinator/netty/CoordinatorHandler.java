0b3822c6c90187df8bc16115acd0fcc169a9487b
huangyonghui <306634799@qq.com>
2021-06-09 14:46:17 +0800

    过滤无效数据

--- coordinator/src/main/java/com/ido/robin/coordinator/netty/CoordinatorHandler.java
+++ coordinator/src/main/java/com/ido/robin/coordinator/netty/CoordinatorHandler.java
@@ -13,16 +13,9 @@ import com.ido.robin.server.controller.dto.PutCmd;
 import com.ido.robin.server.controller.dto.RemoveCmd;
 import com.ido.robin.server.util.RequestUtil;
 import com.ido.robin.sstable.dto.State;
-import io.netty.buffer.ByteBuf;
-import io.netty.buffer.Unpooled;
-import io.netty.channel.ChannelFuture;
-import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.handler.codec.http.FullHttpRequest;
-import io.netty.handler.codec.http.FullHttpResponse;
-import io.netty.handler.codec.http.HttpResponseStatus;
-import io.netty.util.CharsetUtil;
 import io.netty.util.ReferenceCountUtil;
 import lombok.extern.slf4j.Slf4j;
 
@@ -68,7 +61,6 @@ public class CoordinatorHandler extends ChannelInboundHandlerAdapter {
             handleHttpRequest(ctx, (FullHttpRequest) msg);
         }
     }
-
 
 
     static class RemoveNodeCmd {
@@ -124,7 +116,7 @@ public class CoordinatorHandler extends ChannelInboundHandlerAdapter {
                 List<State> states = serverList.stream().map(a -> {
                     DistributedWebServer s = (DistributedWebServer) a;
                     return s.state();
-                }).collect(Collectors.toList());
+                }).filter(a -> a != null && a.getMetas() != null && a.getMetas().get(0).getMetadata().getSegmentFileName() != null).collect(Collectors.toList());
 
                 return RequestUtil.buildJsonRsp(states);
             } else if (route.equals(Route.NODES_INFO)) {
@@ -173,28 +165,6 @@ public class CoordinatorHandler extends ChannelInboundHandlerAdapter {
         return data;
     }
 
-
-    /**
-     * response
-     *
-     * @param ctx
-     * @param request
-     * @param response
-     */
-    private static void sendHttpResponse(ChannelHandlerContext ctx,
-                                         FullHttpRequest request, FullHttpResponse response) {
-        //返回给客户端
-        if (response.status().code() != HttpResponseStatus.OK.code()) {
-            ByteBuf buf = Unpooled.copiedBuffer(response.status().toString(), CharsetUtil.UTF_8);
-            response.content().writeBytes(buf);
-            buf.release();
-        }
-        //如果不是keepalive那么就关闭连接
-        ChannelFuture f = ctx.channel().writeAndFlush(response);
-        if (response.status().code() != HttpResponseStatus.OK.code()) {
-            f.addListener(ChannelFutureListener.CLOSE);
-        }
-    }
 
 }
 
