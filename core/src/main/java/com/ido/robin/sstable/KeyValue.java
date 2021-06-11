e889b71453145109cb0dd27da47602ac1259924c
huangyonghui <306634799@qq.com>
2021-06-11 17:27:19 +0800

    修复过期时间重复添加

--- core/src/main/java/com/ido/robin/sstable/KeyValue.java
+++ core/src/main/java/com/ido/robin/sstable/KeyValue.java
@@ -1,22 +1,23 @@
 package com.ido.robin.sstable;
 
+import lombok.Data;
+
 /**
  * key value pairs
  *
  * @author Ido
  * @date 2019/1/25 15:55
  */
+@Data
 public class KeyValue {
     private String key;
     private String val;
-    private long expiredTime  = Block.PERMANENT;
+    private long expiredTime = Block.PERMANENT;
 
     public KeyValue(String key, String val, long expiredTime) {
         this.key = key;
         this.val = val;
-        if (expiredTime != Block.PERMANENT) {
-            this.expiredTime = System.currentTimeMillis() + expiredTime;
-        }
+        this.expiredTime = expiredTime;
     }
 
     public KeyValue(String key, String val) {
@@ -24,30 +25,4 @@ public class KeyValue {
         this.val = val;
     }
 
-    public String getKey() {
-        return key;
-    }
-
-    public KeyValue setKey(String key) {
-        this.key = key;
-        return this;
-    }
-
-    public String getVal() {
-        return val;
-    }
-
-    public KeyValue setVal(String val) {
-        this.val = val;
-        return this;
-    }
-
-    public long getExpiredTime() {
-        return expiredTime;
-    }
-
-    public KeyValue setExpiredTime(long expiredTime) {
-        this.expiredTime = expiredTime;
-        return this;
-    }
 }
